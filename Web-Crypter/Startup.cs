4e8be29be7144930e4d0d0e4f6d9d22e33b2f448
hamidullahsafi <hamidullah.bsse1890@iiu.edu.pk>
2016-12-31 17:40:40 +0500

    Initial Commet
    Final Year Project OF BSSE

---
+++ Web-Crypter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web_Crypter.Startup))]
+namespace Web_Crypter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
